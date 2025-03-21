f0885f6408091971cfdc046b2354bed1a9b09d1e
thrinaibatchu <thrinaibatchu@gmail.com>
2025-03-21 15:02:44 -0400

    Create leetcode_252_meeting_rooms.py

---
+++ sorting/leetcode_252_meeting_rooms.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        for i in range(len(intervals)):
+            for j in range(i+1, len(intervals)):
+                if (intervals[i][0] >= intervals[j][0] and intervals[i][0] < intervals[j][1] or intervals[j][0] >= intervals[i][0] and intervals[j][0] < intervals[i][1]):
+                    return False
+        
+        return True
+            
