0fb0dfad916140836fb85b84fe0fec81c788e928
thrinaibatchu <thrinaibatchu@gmail.com>
2025-05-16 14:06:09 -0400

    Create leetcode_700_bst.py

---
+++ Trees/leetcode_700_bst.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr = root
+        while curr is not None:
+            if curr.val == val:
+                return curr
+            elif val < curr.val:
+                curr = curr.left
+            else:
+                curr = curr.right
+        
+        return curr
+        
